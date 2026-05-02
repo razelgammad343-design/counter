f05592d37b46fc46abb6115df6f322a0ceed7262
razelgammad343-design <razelgammad343@gmail.com>
2026-05-02 23:10:15 +0800

    Update counter.py

--- counter.py
+++ counter.py
@@ -135,7 +135,7 @@ async def send_live_log(channel, user, pack, value):
     if channel.id == CHANNEL_2:
         u = get_user_data(user.id)
         embed.add_field(
-            name="💰 Profit (WL)",
+            name="💰 Profit :wl: ",
             value=f"{u['profit']:.2f}",
             inline=False
         )
